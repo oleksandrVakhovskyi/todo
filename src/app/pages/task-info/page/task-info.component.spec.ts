6e716c4603f560b216147f8e1ad2d5a67e0b8f2e
oleksandrVakhovskyi <o.vakhovskyi@do-it.co>
2022-02-01 13:22:17 +0200

    add some tests to task info page & tasc-list component

--- src/app/pages/task-info/page/task-info.component.spec.ts
+++ src/app/pages/task-info/page/task-info.component.spec.ts
@@ -1,14 +1,37 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { BackendService } from 'src/app/backend.service';
 
 import { TaskInfoComponent } from './task-info.component';
 
 describe('TaskInfoComponent', () => {
   let component: TaskInfoComponent;
   let fixture: ComponentFixture<TaskInfoComponent>;
-
+  let serviceSpy= jasmine.createSpyObj('Service', {
+    'retrieveData1': of('mock data'),
+    'other': 'some val'
+});
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskInfoComponent ]
+      declarations: [ TaskInfoComponent ],
+      providers: [  
+         BackendService, 
+         {
+          provide: ActivatedRoute,
+          useValue: {
+              snapshot: {
+                  paramMap: {
+                      get(): string {
+                          return '1';
+                      },
+                  },
+              },
+          },
+      },
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
--- src/app/pages/tasks-list/page/tasks-list.component.spec.ts
+++ src/app/pages/tasks-list/page/tasks-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ButtonComponent } from 'src/app/components/button/button.component';
 import { FormComponent } from '../components/form/form.component';
 import { TableComponent } from '../components/table/table.component';
@@ -13,6 +14,8 @@ describe('TasksListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ TasksListComponent  ],
+      providers: [], 
+      imports: [RouterTestingModule],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
