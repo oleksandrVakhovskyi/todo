f20778d233c11d37998fa393486de860a0130248
oleksandrVakhovskyi <o.vakhovskyi@do-it.co>
2022-02-01 09:34:58 +0200

    create first stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {BackendService} from './backend.service';
+import { BackendService } from './backend.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
